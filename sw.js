a34239411e5de4ed6876e237f9491b50865be1bf
Sethjesus <162777479+Sethjesus@users.noreply.github.com>
2025-10-20 10:04:22 +0800

    Refactor service worker caching and logging
    Updated service worker to use new cache version and added logging.

--- sw.js
+++ sw.js
@@ -1,26 +1,18 @@
-const CACHE = "udmgame-pwa-v1";
-const ASSETS = [
-  "/UDMgame/",
-  "/UDMgame/index.html",
-  "/UDMgame/manifest.json",
-  "/UDMgame/192.png",
-  "/UDMgame/512.png"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", e => {
+self.addEventListener('install', e => {
+  console.log('Service Worker installed.');
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
-    )
+    caches.open('jp-esg-cache-v1').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icons/icon-192.png',
+      './icons/icon-512.png'
+    ]))
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
 });
